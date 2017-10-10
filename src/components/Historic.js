e99702a92b11991a02de049366582d9f176a5767
Grayson Hicks <graysonhicks@gmail.com>
2017-10-09 23:20:03 -0400

    starting language chart

--- src/components/Historic.js
+++ src/components/Historic.js
@@ -1,37 +1,53 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "../styles/Historic.css";
 import _ from "underscore";
 
-import { Line } from "react-chartjs-2";
+import {Bar} from "react-chartjs-2";
 
 class Historic extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			historicData: []
-		};
-	}
-	componentDidMount() {
-		this.props.getChartData("historic").then(json => this.setState({ historicData: json }));
-	}
+    this.state = {
+      historicData: {
+        data: []
+      }
+    };
+  }
+  componentDidMount() {
+    this.props.getChartData("historic").then(json => this.setState({historicData: json}));
+  }
 
-	render() {
-		return (
-			<div className="Historic">
-				<Line
-					data={{
-						labels: _.pluck(this.state.historicData.data, "year"),
-						datasets: [
-							{
-								data: [20, 30, 40]
-							}
-						]
-					}}
-				/>
-			</div>
-		);
-	}
+  render() {
+    console.log(this.state);
+    return (
+      <div className="Historic">
+        <Bar data={{
+          datasets: [
+            {
+              data: this.state.historicData.data.map((item, i) => {
+								var o = {
+									x: new Date(item.year),
+									y: i++
+								}
+								return o;
+							})
+            },
+						{
+							data: this.state.historicData.data.map((item)=> item.language)
+						}
+          ]
+        }} options={{
+        scales: {
+            xAxes: [{
+
+                labels: _.pluck(this.state.historicData.data, "year"),
+            }]
+        }
+    }}/>
+      </div>
+    );
+  }
 }
 
 export default Historic;
