ffd0760f6904900ab94a54d6ffda51fea80d6fae
Grayson Hicks <graysonhicks@gmail.com>
2017-10-10 10:24:26 -0400

    created language chart basically working

--- src/components/Historic.js
+++ src/components/Historic.js
@@ -1,53 +1,71 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../styles/Historic.css";
 import _ from "underscore";
 
-import {Bar} from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 
 class Historic extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      historicData: {
-        data: []
-      }
-    };
-  }
-  componentDidMount() {
-    this.props.getChartData("historic").then(json => this.setState({historicData: json}));
-  }
+		this.state = {
+			historicData: {
+				data: []
+			}
+		};
+	}
+	componentDidMount() {
+		this.props.getChartData("historic").then(json => this.setState({ historicData: json }));
+	}
 
-  render() {
-    console.log(this.state);
-    return (
-      <div className="Historic">
-        <Bar data={{
-          datasets: [
-            {
-              data: this.state.historicData.data.map((item, i) => {
-								var o = {
-									x: new Date(item.year),
-									y: i++
+	render() {
+		var uniqueYears = [];
+		uniqueYears = _.uniq(_.pluck(this.state.historicData.data, "year"));
+		var languages = [];
+		languages = this.state.historicData.data.map(item => {
+			var years = [];
+			for (var i = 0; i < uniqueYears.length; i++) {
+				if (item.year == uniqueYears[i]) {
+					years.push(1);
+				} else {
+					years.push(0);
+				}
+			}
+			return {
+				label: item.language,
+				backgroundColor: "#" + Math.floor(Math.random() * 16777215).toString(16),
+				data: years
+			};
+		});
+		console.log(languages);
+		return (
+			<div className="Historic">
+				<Bar
+					data={{
+						labels: uniqueYears,
+						datasets: languages
+					}}
+					options={{
+						legend: {
+							display: false
+						},
+						scales: {
+							yAxes: [
+								{
+									stacked: true
 								}
-								return o;
-							})
-            },
-						{
-							data: this.state.historicData.data.map((item)=> item.language)
+							],
+							xAxes: [
+								{
+									stacked: true
+								}
+							]
 						}
-          ]
-        }} options={{
-        scales: {
-            xAxes: [{
-
-                labels: _.pluck(this.state.historicData.data, "year"),
-            }]
-        }
-    }}/>
-      </div>
-    );
-  }
+					}}
+				/>
+			</div>
+		);
+	}
 }
 
 export default Historic;
