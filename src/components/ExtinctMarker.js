db4969e1b24a81e0ed81f9e40beb62fc625701f1
Grayson Hicks <graysonhicks@gmail.com>
2017-10-20 08:45:07 -0400

    extinct icon

--- src/components/ExtinctMarker.js
+++ src/components/ExtinctMarker.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Marker, InfoWindow } from "react-google-maps";
 
+import dead from "../images/pirates.png";
+
 const spreadMarkers = function(position) {
-	return position * (Math.random() * (1.000001 - 0.9) + 0.9);
+	return position * (Math.random() * (1.000001 - 0.98) + 0.98);
 };
 
 class ExtinctMarker extends Component {
@@ -40,11 +42,14 @@ class ExtinctMarker extends Component {
 				id={this.props.marker._id}
 				style={{ height: "5px", width: "5px", margin: "5px" }}
 				className={"marker"}
+				icon={{
+					url: dead
+				}}
 			>
 				{this.state.isOpen ? (
 					<InfoWindow onCloseClick={this.onToggleOpen}>
 						<div>
-							Language: {this.props.marker.language} <br />Status: {this.props.marker["Degree of endangerment"]}
+							Language: {this.props.marker.language} <br />Date of Extinction: {this.props.marker["year"] || "Unknown"}
 						</div>
 					</InfoWindow>
 				) : (
